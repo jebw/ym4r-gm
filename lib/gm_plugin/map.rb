1aecdfedfd2a6ce085c6001463dc526416a19f1d
jebw <jeb@jdwilkins.co.uk>
2010-10-18 10:43:07 +0100

    just allow asset path to be passed from options
    can't see how to access config.asset_path otherwise

--- lib/gm_plugin/map.rb
+++ lib/gm_plugin/map.rb
@@ -33,7 +33,7 @@ module Ym4r
         options[:sensor] = false unless options.has_key?(:sensor)
         api_key = ApiKey.get(options)
         a = "<script src=\"http://maps.google.com/maps?file=api&amp;v=2.x&amp;key=#{api_key}&amp;hl=#{options[:hl]}&amp;sensor=#{options[:sensor]}\" type=\"text/javascript\"></script>\n"
-        a << "<script src=\"/javascripts/ym4r-gm.js\" type=\"text/javascript\"></script>\n" unless options[:without_js]
+        a << "<script src=\"#{options[:asset_path]}/javascripts/ym4r-gm.js\" type=\"text/javascript\"></script>\n" unless options[:without_js]
         a << "<style type=\"text/css\">\n v\:* { behavior:url(#default#VML);}\n</style>" if options[:with_vml]
         a << "<script src=\"http://www.google.com/uds/api?file=uds.js&amp;v=1.0\" type=\"text/javascript\"></script>" if options[:local_search]
         a << "<script src=\"http://www.google.com/uds/solutions/localsearch/gmlocalsearch.js\" type=\"text/javascript\"></script>\n" if options[:local_search]
